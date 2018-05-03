9b2b111551fa8fc65a8fbe96dddf45fd5b20067a
joe-re <joe.tialtngo@gmail.com>
2018-05-04 04:20:39 +0900

    enable to complete from and where keywords

--- src/complete.ts
+++ src/complete.ts
@@ -1,17 +1,26 @@
 import { Parser, AstReader } from '@joe-re/node-sql-parser'
 
+const CLAUSES = ['SELECT', 'FROM', 'WHERE', 'ORDER BY', 'GROUP BY', 'LIMIT']
+
 function extractExpectedLiterals(expected: { type: string, text: string }[]): string[] {
   return expected.filter(v => v.type === 'literal')
 	  .map(v => v.text)
 		.filter((v, i, self) => self.indexOf(v) === i)
 }
 
+function getLastToken(sql: string) {
+  const match = sql.match(/^.*[\s|.](.*?)$/)
+	if (!match) { return sql }
+	return match[1]
+}
+
 export default function complete(sql: string, pos: { line: number, column: number }) {
   let candidates: string[] = []
-	let error = {};
+	let error = null;
   try {
     const ast = Parser.parse(sql);
     const ar  = new AstReader(ast);
+		candidates = CLAUSES
   } catch (e) {
 	  candidates = extractExpectedLiterals(e.expected)
 		error = {
@@ -21,6 +30,7 @@ export default function complete(sql: string, pos: { line: number, column: numbe
       offset: e.offset
     }
   }
-	candidates = candidates.filter(v => v.startsWith(sql))
+  const lastToken = getLastToken(sql)
+	candidates = candidates.filter(v => v.startsWith(lastToken))
 	return { candidates, error }
 }
--- test/complete.ts
+++ test/complete.ts
@@ -1,8 +1,20 @@
 import test from 'ava'
 import complete from '../src/complete'
 
-test("complete 'select' key word", async (t) => {
+test("complete 'SELECT' key word", async (t) => {
   const result = complete('S', { line: 1, column: 1 })
 	t.is(result.candidates.length, 1);
 	t.is(result.candidates[0], 'SELECT');
+});
+
+test("complete 'FROM' key word", async (t) => {
+  const result = complete('SELECT * F', { line: 1, column: 10 })
+	t.is(result.candidates.length, 1);
+	t.is(result.candidates[0], 'FROM');
+});
+
+test("complete 'WHERE' key word", async (t) => {
+  const result = complete('SELECT * FROM FOO W', { line: 1, column: 10 })
+	t.is(result.candidates.length, 1);
+	t.is(result.candidates[0], 'WHERE');
 });
