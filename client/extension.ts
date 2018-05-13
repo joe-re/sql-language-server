21a06b6b8ec93bbd41c02d42e91aadc884cfa392
joe-re <joe.tialtngo@gmail.com>
2018-05-13 18:44:57 +0900

    change server name

--- client/extension.ts
+++ client/extension.ts
@@ -1,9 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-'use strict';
-
 import * as path from 'path';
 
 import { workspace, ExtensionContext } from 'vscode';
@@ -11,34 +5,23 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 
 export function activate(context: ExtensionContext) {
 
-	// The server is implemented in node
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
-	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-	
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
+
 	let serverOptions: ServerOptions = {
 		run : { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 	}
-	
-	// Options to control the language client
+
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
 		documentSelector: [{scheme: 'file', language: 'plaintext'}],
 		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: 'lspSample',
-			// Notify the server about file changes to '.clientrc files contain in the workspace
+			configurationSection: 'sqlLanguageServer',
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
-	
-	// Create the language client and start the client.
-	let disposable = new LanguageClient('lspSample', 'Language Server Example', serverOptions, clientOptions).start();
-	
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
+
+	let disposable = new LanguageClient('sqlLanguageServer', 'SQL Language Server', serverOptions, clientOptions).start();
+
 	context.subscriptions.push(disposable);
 }
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-'use strict';
-
 import {
 	IPCMessageReader, IPCMessageWriter, createConnection, IConnection, TextDocuments, TextDocument,
 	Diagnostic, DiagnosticSeverity, InitializeResult, TextDocumentPositionParams, CompletionItem,
@@ -24,25 +18,17 @@ const logger = log4js.getLogger()
 // Create a connection for the server. The connection uses Node's IPC as a transport
 let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
 let documents: TextDocuments = new TextDocuments();
-// Make the text document manager listen on the connection
-// for open, change and close text document events
 documents.listen(connection);
 
 
 let shouldSendDiagnosticRelatedInformation: boolean = false;
 
-// After the server has started the client sends an initialize request. The server receives
-// in the passed params the rootPath of the workspace plus the client capabilities.
 connection.onInitialize((_params): InitializeResult => {
 	shouldSendDiagnosticRelatedInformation = _params.capabilities && _params.capabilities.textDocument && _params.capabilities.textDocument.publishDiagnostics && _params.capabilities.textDocument.publishDiagnostics.relatedInformation;
 	return {
 		capabilities: {
-			// Tell the client that the server works in FULL text document sync mode
 			textDocumentSync: documents.syncKind,
-			// Tell the client that the server support code complete
 			completionProvider: {
 				resolveProvider: true,
         triggerCharacters: ['.'],
@@ -60,12 +46,12 @@ connection.onInitialize((_params): InitializeResult => {
 
 // The settings interface describe the server relevant settings part
 interface Settings {
-	lspSample: ExampleSettings;
+	sqlLanguageServer: SqlLanguageServerSettings;
 }
 
 // These are the example settings we defined in the client's package.json
 // file
-interface ExampleSettings {
+interface SqlLanguageServerSettings {
 	maxNumberOfProblems: number;
 }
 
@@ -75,7 +61,7 @@ let maxNumberOfProblems: number;
 // as well.
 connection.onDidChangeConfiguration((change) => {
 	let settings = <Settings>change.settings;
-	maxNumberOfProblems = settings.lspSample.maxNumberOfProblems || 100;
+	maxNumberOfProblems = settings.sqlLanguageServer.maxNumberOfProblems || 100;
 	// Revalidate any open text documents
 	documents.all().forEach(validateTextDocument);
 });
@@ -153,30 +139,9 @@ connection.onCompletion((docParams: TextDocumentPositionParams): CompletionItem[
 	}, [ { table: 'USERS', columns: ['id', 'email', 'created_at', 'updated_at'] }]).candidates
 	logger.debug(candidates.join(","))
 	return candidates.map(v => ({ label: v, kind: CompletionItemKind.Text }))
-	// return [
-	// 	{
-	// 		label: 'TypeScript',
-	// 		kind: CompletionItemKind.Text,
-	// 		data: 1
-	// 	},
-	// 	{
-	// 		label: 'JavaScript',
-	// 		kind: CompletionItemKind.Text,
-	// 		data: 2
-	// 	}
-	// ]
 });
 
-// This handler resolve additional information for the item selected in
-// the completion list.
 connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  // if (item.data === 1) {
-  //   item.detail = 'TypeScript details dedede',
-  //   item.documentation = 'TypeScript documentation'
-  // } else if (item.data === 2) {
-  //   item.detail = 'JavaScript details',
-  //   item.documentation = 'JavaScript documentation'
-  // }
   return item;
 });
 
