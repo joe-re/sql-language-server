48607220b09e9e91774b2ecd109bd5fcc1b04203
joe-re <joe.tialtngo@gmail.com>
2020-06-14 20:17:34 +0800

    implement fix on whereClauseNewLine

--- packages/sqlint/src/rules/whereClauseNewLine.ts
+++ packages/sqlint/src/rules/whereClauseNewLine.ts
@@ -13,24 +13,41 @@ export const whereClauseNewLine: Rule<SelectStatement, RuleConfig<Options>> = {
     if (!context.node.where) {
       return
     }
-    function findInvalidClause(expr: BinaryExpressionNode): BinaryExpressionNode | null {
+    function findInvalidClauses(
+      expr: BinaryExpressionNode,
+      invalidClauses: BinaryExpressionNode[] = []
+    ): BinaryExpressionNode[] {
       if (expr.left.location.start.line === expr.right.location.start.line && expr.left.type === 'binary_expr') {
-        return expr.left as BinaryExpressionNode
+        invalidClauses.push(expr.left as BinaryExpressionNode)
       }
       if (expr.right.type === 'binary_expr') {
-        return findInvalidClause(expr.right as BinaryExpressionNode)
+        return findInvalidClauses(expr.right as BinaryExpressionNode, invalidClauses)
       }
-      return null
+      return invalidClauses
     }
 
-    const invalidClause = findInvalidClause(context.node.where.expression)
-    if (invalidClause) {
-      return {
-        message: 'Multiple where clause must go on a new line.',
-        location: invalidClause.location,
-        rulename: META.name,
-        errorLevel: context.config.level
-      }
+    const invalidClauses = findInvalidClauses(context.node.where.expression)
+    if (invalidClauses.length === 0) {
+      return
     }
+    return invalidClauses.map(v => ({
+      message: 'Multiple where clause must go on a new line.',
+      location: v.location,
+      rulename: META.name,
+      errorLevel: context.config.level,
+      fix: (fixer) => {
+        const afterSpaces = context.getAfterSQL(v.location).match(/^\s+/)
+        console.log('--- afterSql ---')
+        console.log(context.getAfterSQL(v.location))
+        const afterSpaceNumber = afterSpaces ? afterSpaces[0].length : 0
+        console.log('--- afterSpaceNumber ---')
+        console.log(afterSpaceNumber)
+        const needSpaces = v.location.start.column - afterSpaceNumber
+        return [
+          fixer.replaceText(v.location.end.offset, v.location.end.offset + 1, '\n'),
+          fixer.insertText(v.location.end.offset + 1, ''.padStart(needSpaces, ' '))
+        ]
+      }
+    }))
   }
 }
--- packages/sqlint/test/rules/whereClauseNewLine.test.ts
+++ packages/sqlint/test/rules/whereClauseNewLine.test.ts
@@ -1,4 +1,5 @@
 import { execute } from '../../src/rules'
+import { applyFixes } from '../testUtil'
 
 test('valid case', () => {
   const sql = `
@@ -17,22 +18,29 @@ test('Multiple clauses must go on a new line', () => {
   const sql = `
     SELECT foo.a, foo.b
     FROM foo
-    WHERE
-      foo.a = 'a' AND foo.b = 'b'
+    WHERE foo.a = 'a' AND foo.b = 'b'
   `
   const result = execute(sql, { rules: { 'where-clause-new-line': { level: 2 } } })
   expect(result.length).toEqual(1)
   expect(result[0].message).toEqual('Multiple where clause must go on a new line.')
   expect(result[0].location).toEqual({
     start: {
-      column: 7,
-      line: 5,
-      offset: 54 
+      column: 11,
+      line: 4,
+      offset: 48 
     },
     end: {
-      column: 18,
-      line: 5,
-      offset: 65 
+      column: 22,
+      line: 4,
+      offset: 59
     }
   })
+ const fixed = applyFixes(sql, result.map(v => v.fix!))
+ console.log(fixed)
+ expect(fixed).toContain(`
+    SELECT foo.a, foo.b
+    FROM foo
+    WHERE foo.a = 'a'
+          AND foo.b = 'b'
+`)
 })
