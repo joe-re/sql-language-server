195da41079eaa1a8b75d356b7ecab4520391131d
joe-re <joe.tialtngo@gmail.com>
2020-08-16 11:26:38 +0000

    deal with subquery in where clause

--- packages/sql-parser/base/fromClauseParser.js
+++ packages/sql-parser/base/fromClauseParser.js
@@ -5055,7 +5055,10 @@ function peg$parse(input, options) {
                 s0 = peg$FAILED;
               }
               if (s0 === peg$FAILED) {
-                s0 = peg$parsevar_decl();
+                s0 = peg$parseselect_stmt();
+                if (s0 === peg$FAILED) {
+                  s0 = peg$parsevar_decl();
+                }
               }
             }
           }
