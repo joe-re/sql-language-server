7836fc959795fe437a757fbcf9f1411c327398ae
Leko <leko.noor@gmail.com>
2021-09-24 10:30:24 +0800

    feat: add BigQueryClient as a database lib

--- packages/server/src/database_libs/getDatabaseClient.ts
+++ packages/server/src/database_libs/getDatabaseClient.ts
@@ -3,6 +3,7 @@ import MysqlClient from './MysqlClient'
 import PostgresClient from './PostgresClient'
 import { Connection } from '../SettingStore'
 import Sqlite3Client from './Sqlite3Client'
+import BigqueryClient from './BigqueryClient'
 
 export default function getDatabaseClient(settings: Connection): AbstractClient {
   switch (settings.adapter) {
@@ -10,6 +11,7 @@ export default function getDatabaseClient(settings: Connection): AbstractClient 
     case 'postgres': return new PostgresClient(settings)
     case 'postgresql': return new PostgresClient(settings)
     case 'sqlite3': return new Sqlite3Client(settings)
+    case 'bigquery': return new BigqueryClient(settings)
     default: throw new Error(`not support ${settings.adapter}`)
   }
 }
